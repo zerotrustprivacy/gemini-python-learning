72f39879fa4765ebec1b9bdbdb09ef7f02b42d61
zerotrustprivacy <hendersonmersedes@gmail.com>
2025-07-30 11:10:29 -0500

    challenge files

---
+++ solution.py
@@ -0,0 +1,13 @@
+# Solution: Variables and Data Types
+
+# A string variable named `first_name`
+first_name = "Ada"
+
+# An integer variable named `your_age`
+your_age = 30
+
+# A float variable named `account_balance`
+account_balance = 150.
+
+# A boolean variable named `is_active_user`
+is_active_user = True
